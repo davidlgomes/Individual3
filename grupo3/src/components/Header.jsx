9701f3f0d3d1358390fa02028825c5a637388fc1
Devair Martins <devair.martins@gmail.com>
2023-02-07 14:17:48 -0300

    rotas

--- grupo3/src/components/Header.jsx
+++ grupo3/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 
 function Header() {
   return (
@@ -12,6 +14,10 @@ function Header() {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Button variant="primary">
+              Vasco <Badge bg="secondary">9</Badge>
+              <span className="visually-hidden">unread messages</span>
+            </Button>
           </Nav>
         </Container>
       </Navbar>
--- grupo3/src/main.jsx
+++ grupo3/src/main.jsx
@@ -1,24 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
+import App from './App'
 // import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Header from './components/header';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Está funcionando</div>,
+    element: <App/>,
+    errorElement: <h1>Erro 404. Está pág. não existe!</h1>,
+    children:[
+      {
+        path: "/",
+        element: <h1>HOME</h1>
+      },
+      {
+        path: "/",
+        element: <h1>HOME</h1>
+      },
+      {
+        path: "/cardapio",
+        element: <h2>devair</h2>
+      },
+      {
+        path: "/pedido",
+        element: <h1>PEDIDOS</h1>
+      },
+      {
+        path: "/carrinho",
+        element: <h1>CARRINHO</h1>
+      },
+      {
+        path: "/nos",
+        element: <h1>NÓS</h1>
+      }
+    ]
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header/>
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
