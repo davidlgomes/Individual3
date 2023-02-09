6660ff529af9c8b0a061f2c4a6f356a2bcb4b9ee
DEVair Martins <83782674+DevairUva@users.noreply.github.com>
2023-02-09 15:56:38 -0300

    Update Header.jsx

--- grupo3/src/components/header/Header.jsx
+++ grupo3/src/components/header/Header.jsx
@@ -11,11 +11,11 @@ function Header() {
         <Container className='navBarColor'>
           <Navbar.Brand href="/"  className='navBarColor'>Navbar</Navbar.Brand>
           <Nav>
-            <Nav><Link to="/" className='textColor'>Home</Link></Nav>
-            <Nav><Link to="/cardapio" className='textColor'>Cardápio</Link></Nav>
-            <Nav><Link to="/pedido" className='textColor'>Pedidos</Link></Nav>
-            <Nav><Link to="/nos" className='textColor'>Contato</Link></Nav>
-            <Nav><Link to="/carrinho" className='textColor'><i className="fa fa-shopping-cart"></i><Badge bg="secondary">0</Badge></Link></Nav>
+            <Nav><Link to="/" className='links'>Home</Link></Nav>
+            <Nav><Link to="/cardapio" className='links'>Cardápio</Link></Nav>
+            <Nav><Link to="/pedido" className='links'>Pedidos</Link></Nav>
+            <Nav><Link to="/nos" className='links'>Contato</Link></Nav>
+            <Nav><Link to="/carrinho" className='links'><i className="fa fa-shopping-cart"></i><Badge bg="secondary">0</Badge></Link></Nav>
           </Nav>
         </Container>
       </Navbar>
