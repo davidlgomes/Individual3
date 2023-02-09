5df1fb63fc0032e30eb9b4665d163e47bc893060
Devair Martins <devair.martins@gmail.com>
2023-02-09 13:58:53 -0300

    borda rosa

--- grupo3/src/components/cardapio/Pratos.jsx
+++ grupo3/src/components/cardapio/Pratos.jsx
@@ -13,7 +13,7 @@ function Pratos() {
             <div>
                 <h5>Hambúrgueres</h5>
                 <div className='cards'>
-                    <Card style={{ width: '19rem' }}>
+                    <Card style={{ width: '19rem'}} className='rosa'>
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
                             <Card.Title className='textoCentro'>X-Bacon</Card.Title>
@@ -22,12 +22,12 @@ function Pratos() {
                                 <br />
                                 <h5>Preço R$ 15,00</h5>
                             </Card.Text>
-                            <div className='textoCentro'>
+                            {/* <div className='textoCentro'>
                                 <Button variant="primary">R$ 15,00</Button>
-                            </div>
+                            </div> */}
                         </Card.Body>
                     </Card>
-                    <Card style={{ width: '19rem' }}>
+                    <Card style={{ width: '19rem'}} className='rosa'>
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
                         <Card.Title className='textoCentro'>X-Burger</Card.Title>
@@ -36,12 +36,12 @@ function Pratos() {
                                 <br />
                                 <h5>Preço R$ 15,00</h5>
                             </Card.Text>
-                            <div className='textoCentro'>
+                            {/* <div className='textoCentro'>
                                 <Button variant="primary">R$ 15,00</Button>
-                            </div>
+                            </div> */}
                         </Card.Body>
                     </Card>
-                    <Card style={{ width: '19rem' }}>
+                    <Card style={{ width: '19rem'}} className='rosa'>
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
                         <Card.Title className='textoCentro'>Vegetariano</Card.Title>
@@ -50,9 +50,9 @@ function Pratos() {
                                 <br />
                                 <h5>Preço R$ 15,00</h5>
                             </Card.Text>
-                            <div className='textoCentro'>
+                            {/* <div className='textoCentro'>
                                 <Button variant="primary">R$ 15,00</Button>
-                            </div>
+                            </div> */}
                         </Card.Body>
                     </Card>
                 </div>
@@ -60,120 +60,120 @@ function Pratos() {
                 <h5>Bebidas</h5>
                 <div>
                     <div className='cards'>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Coca</Card.Title>
-                                <Card.Text>
+                            <Card.Title className='textoCentro'>Coca</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     O mais querido de todos.
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Guaraná</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='textoCentro'>Guaraná</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     Sempre em segundo.
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Pepsi</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='textoCentro'>Pepsi</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     Pode ser?
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
                     </div>
                     <br />
                     <h5>Acompanhamentos</h5>
                     <div className='cards'>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Batata Frita</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='textoCentro'>Batata Frita</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     Batata frita com chedar e bacon.
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Tekitos</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='textoCentro'>Tekitos</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     Tipo Mini-chiquen.
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Cebola Empanada</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='textoCentro'>Cebola Empanada</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     Bora pra chorar.
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
                     </div>
                     <br />
                     <h5>Sobremesas</h5>
                     <div className='cards'>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Sorvete</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='textoCentro'>Sorvete</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     Tradicional sabor napolitano.
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Açaí</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='textoCentro'>Açaí</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     Não é aquele que parece sorvete.
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
-                        <Card style={{ width: '19rem' }}>
+                        <Card style={{ width: '19rem'}} className='rosa'>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Salada de Frutas</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='textoCentro'>Salada de Frutas</Card.Title>
+                                <Card.Text className='textoCentro'>
                                     Maçã, banana, mamão e laranja.
                                     <br />
                                     <h5>Preço R$ 15,00</h5>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
                             </Card.Body>
                         </Card>
                     </div>
